51c4398faf5713f2cc2e2a4e752750bd515ecf02
Giotis Kal <giotisgr@gmail.com>
2016-03-27 14:33:21 +0300

    Added a Filterizr usecase stripped to bare minimums for development purposes

---
+++ dev_example/js/controls.js
@@ -0,0 +1,16 @@
+$(function() {
+    //Simple filter controls
+    $('.simplefilter li').click(function() {
+        $('.simplefilter li').removeClass('active');
+        $(this).addClass('active');
+    });
+    //Multifilter controls
+    $('.multifilter li').click(function() {
+        $(this).toggleClass('active');
+    });
+    //Sort controls
+    $('.sort-btn').click(function() {
+        $('.sort-btn').removeClass('active');
+        $(this).addClass('active');
+    });
+});
