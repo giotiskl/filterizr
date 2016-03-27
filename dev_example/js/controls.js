6c9e8be93cf9ed9c7bec24304c9450e15d53f2f4
Giotis Kal <giotisgr@gmail.com>
2016-03-27 14:46:02 +0300

    Added search control to dev_example

--- dev_example/js/controls.js
+++ dev_example/js/controls.js
@@ -8,6 +8,10 @@ $(function() {
     $('.multifilter li').click(function() {
         $(this).toggleClass('active');
     });
+    //Shuffle control
+    $('.shuffle-btn').click(function() {
+        $('.sort-btn').removeClass('active');
+    });
     //Sort controls
     $('.sort-btn').click(function() {
         $('.sort-btn').removeClass('active');
