2d3588d06c04a2a6a26e414a73d7b688d265db1b
Yiotis Kaltsikis <giotisgr@gmail.com>
2017-08-05 08:26:52 +0200

    set up project base for filterizr revisited

---
+++ revisited/app/index.js
@@ -0,0 +1,5 @@
+import Filterizr from './filterizr';
+import DefaultOptions from './options';
+
+const fltr = new Filterizr('.filtr-container', DefaultOptions);
+window.fltr = fltr;
---
+++ revisited/app/options.js
@@ -0,0 +1,27 @@
+const options = {
+  animationDuration: 0.5,
+  callbacks: {
+    onFilteringStart: function() { },
+    onFilteringEnd: function() { },
+    onShufflingStart: function() { },
+    onShufflingEnd: function() { },
+    onSortingStart: function() { },
+    onSortingEnd: function() { }
+  },
+  delay: 0,
+  delayMode: 'progressive',
+  easing: 'ease-out',
+  filter: 'all',
+  filterOutCss: {
+    'opacity': 0,
+    'transform': 'scale(0.5)'
+  },
+  filterInCss: {
+    'opacity': 1,
+    'transform': 'scale(1)'
+  },
+  layout: 'sameSize',
+  setupControls: true,
+}
+
+export default options;
