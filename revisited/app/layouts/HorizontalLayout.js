6868cb05adfcc03870d0c2de394fe82200d5feaf
Yiotis Kaltsikis <giotisgr@gmail.com>
2017-08-08 23:10:55 +0200

    make layouts modular, implement vertical and horizontal layout

---
+++ revisited/app/layouts/HorizontalLayout.js
@@ -0,0 +1,38 @@
+import { map } from 'lodash';
+
+/**
+ * Horizontal layout algorithm that arranges all FilterItems in one row. Their width may vary.
+ * @param {object} Filterizr instance.
+ */
+const HorizontalLayout = (Filterizr) => {
+  const {
+    FilterContainer,
+    FilterItems
+  } = Filterizr.props;
+  let row = 1, 
+      left = 0, 
+      // target height of FilterContainer
+      containerHeight = 0;
+
+  const targetPositions = map(FilterItems, (FilterItem, index) => {
+    const pos = {
+      left: left,
+      top: 0,
+    }
+
+    // update left for next item
+    left += FilterItem.props.w;
+    // check if target height of FilterContainer should be increased
+    if (FilterItem.props.h > containerHeight)
+      containerHeight = FilterItem.props.h;
+
+    return pos;
+  });
+
+  // update the height of the FilterContainer
+  FilterContainer.updateHeight(containerHeight);
+
+  return targetPositions;
+}
+
+export default HorizontalLayout;
---
+++ revisited/app/layouts/SameSizeLayout.js
@@ -0,0 +1,35 @@
+import { map } from 'lodash';
+
+/**
+ * Same size layout for items that have the same width/height
+ * @param {object} Filterizr instance.
+ */
+const SameSizeLayout = (Filterizr) => {
+  const {
+    FilterContainer,
+    FilterItems
+  } = Filterizr.props;
+  // calculate number of columns and rows the grid should have
+  let cols = FilterContainer.calcColumns(Layout);
+  let row = 0;
+  // calculate array of positions
+  const targetPositions = map(FilterItems, (FilterItem, index) => {
+    // update current row
+    if (index % cols === 0 && index >= cols)
+      row++;
+    // determine pos in grid
+    const spot = index - (cols * row);
+    // return object with new position in array
+    return {
+      left: spot * FilterItem.props.w,
+      top: row * FilterItem.props.h,
+    }
+  });
+  // update the height of the FilterContainer
+  // before returning from the method
+  FilterContainer.updateHeight((row+1) * FilterItems[0].props.h);
+  // return the array of new positions
+  return targetPositions;
+}
+
+export default SameSizeLayout;
