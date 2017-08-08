e627556dc2129c2e02fcee7ed85c1ef49d1559b2
Yiotis Kaltsikis <giotisgr@gmail.com>
2017-08-08 23:10:55 +0200

    make layouts modular, implement vertical and horizontal layout

--- revisited/app/Positions.js
+++ revisited/app/Positions.js
@@ -1,42 +1,25 @@
-import { map } from 'lodash';
+import HorizontalLayout from './layouts/HorizontalLayout';
+import VerticalLayout from './layouts/VerticalLayout';
+import SameSizeLayout from './layouts/SameSizeLayout';
 
 /**
  * Calculates and returns an array of objects representing
  * the next positions the FilterItems are supposed to assume.
+ * @param {string} name of layout to decide the helper method to be used
  * @param {object} container is the FilterContainer
  */
 const Positions = (Layout, Filterizr) => {
   switch(Layout) {
+    case 'horizontal':
+      return HorizontalLayout(Filterizr);
+    case 'vertical':
+      return VerticalLayout(Filterizr);
     case 'sameSize':
-      // extract FilterContainer and FilterItems
-      // from the Filterizr instance
-      const {
-        FilterContainer,
-        FilterItems
-      } = Filterizr.props;
-
-      // calculate number of columns and rows the grid should have
-      let cols = FilterContainer.calcColumns(Layout);
-      let row = 0;
-      // calculate array of positions
-      const targetPositions = map(FilterItems, (FilterItem, index) => {
-        // update current row
-        if (index % cols === 0 && index >= cols)
-          row++;
-        // determine pos in grid
-        const spot = index - (cols * row);
-        // return object with new position in array
-        return {
-          left: spot * FilterItem.props.w,
-          top: row * FilterItem.props.h,
-        }
-      });
-      // update the height of the FilterContainer
-      // before returning from the method
-      FilterContainer.updateHeight((row+1) * FilterItems[0].props.h);
-      // return the array of new positions
-      return targetPositions;
+      return SameSizeLayout(Filterizr);
+    default:
+      return SameSizeLayout(Filterizr);
   }
 }
+
 
 export default Positions;
