035aec1a3bf5fe51fe106c76a16c63a4ff40412b
Yiotis Kaltsikis <giotisgr@gmail.com>
2017-08-10 22:06:13 +0200

    parameterize webpack config setup and add analyze script to package.json

--- revisited/webpack.config.js
+++ revisited/webpack.config.js
@@ -3,38 +3,44 @@ var ProvidePlugin = require('webpack').ProvidePlugin;
 var BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 var UglifyJsPlugin = require('webpack').optimize.UglifyJsPlugin;
 
-module.exports = {
-  entry: [
-    // set our app.js as the entry point
-    './app/index',
-  ],
-  output: {
-    path: __dirname,
-    filename: './dist/filterizr.min.js',
-  },
-  module: {
-    loaders: [
-      // JavaScript loader
-      {
-        test: /\.js$/,
-        exclude: /(node_modules|bower_components)/,
-        // run JS through Babel
-        use: {
-          loader: 'babel-loader'
-        }
-      },
-    ]
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      title: "Filterizr revisited",
-      template: "showcase/index.html",
-    }),
-    // uglify plugin for JS
-    new UglifyJsPlugin(),
-    new BundleAnalyzerPlugin(),
-  ],
-  resolve: {
-    extensions: ['.js'],
+module.exports = env => {
+  let config = {
+    entry: [
+      // set our app.js as the entry point
+      './app/index',
+    ],
+    output: {
+      path: __dirname,
+      filename: './dist/filterizr.min.js',
+    },
+    module: {
+      loaders: [
+        // JavaScript loader
+        {
+          test: /\.js$/,
+          exclude: /(node_modules|bower_components)/,
+          // run JS through Babel
+          use: {
+            loader: 'babel-loader'
+          }
+        },
+      ]
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        title: "Filterizr revisited",
+        template: "showcase/index.html",
+      }),
+      // uglify plugin for JS
+      new UglifyJsPlugin(),
+    ],
+    resolve: {
+      extensions: ['.js'],
+    }
   }
+
+  if (env === 'analyze')
+    config.plugins.push(new BundleAnalyzerPlugin());
+
+  return config;
 }
