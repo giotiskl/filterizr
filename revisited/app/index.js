5ba5894ebe04e737f43c1d6d576339053fe9c9a2
Yiotis Kaltsikis <giotisgr@gmail.com>
2017-08-12 19:20:56 +0200

    implement .filterizr method on the jquery object

--- revisited/app/index.js
+++ revisited/app/index.js
@@ -1,6 +1,52 @@
 import Filterizr from './filterizr';
 import DefaultOptions from './options';
 
-const fltr = new Filterizr('.filtr-container', DefaultOptions);
-window.fltr = fltr;
-window.$ = jQuery;
+(function($) {
+  //Make sure jQuery exists
+  if (!$) throw new Error('Filterizr requires jQuery to work.');
+
+  // Extract .filterizr method on jQuery prototype
+  $.fn.filterizr = function() {
+    const selector  = `.${this.get(0).className}`;
+    const args = arguments;
+
+    console.log(this)
+
+
+    // user is instantiating Filterizr
+    if (!this._fltr && args.length === 0 || (args.length === 1 && typeof args[0] === 'object')) {
+      const options = args.length > 0 ? args[0] : DefaultOptions;
+      this._fltr = new Filterizr(selector, options);
+    }
+    // otherwise call the method called
+    else if (args.length >= 1 && typeof args[0] === 'string') {
+      const method = args[0];
+      const methodArgs = Array.prototype.slice.call(args, 1);
+      const filterizr = this._fltr;
+      switch(method) {
+        case 'filter':
+          filterizr.filter(...methodArgs);
+          return this;
+        case 'toggleFilter':
+          filterizr.toggleFilter(...methodArgs);
+          return this;
+        case 'sort':
+          filterizr.sort(...methodArgs);
+          return this;
+        case 'shuffle':
+          filterizr.shuffle(...methodArgs);
+          return this;
+        case 'search':
+          filterizr.search(...methodArgs);
+          return this;
+        case 'setOptions':
+          filterizr.setOptions(...methodArgs);
+          return this;
+        default:
+          return;
+      }
+    }
+
+    return this;
+  }
+})(jQuery);
