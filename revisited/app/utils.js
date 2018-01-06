ba50e9729c44183a2b4329c36fe71e8dc137af81
Yiotis Kaltsikis <giotisgr@gmail.com>
2018-01-06 13:14:14 +0100

    Replace lodash shuffle for fisher yates ES6 implementation

--- revisited/app/utils.js
+++ revisited/app/utils.js
@@ -85,3 +85,24 @@ const debounce = function (func, wait, immediate) {
 };
 
 export { debounce };
+
+/**
+ * Fisher-Yates shuffle ES6 non-mutating implementation.
+ * @param {Array} array the array to shuffle
+ * @return {Array} shuffled array without mutating the initial array.
+ */
+const shuffle = (array) => {
+  // perform deep clone on array to mutate
+  let cloned = array.slice(0);
+  // array to return
+  let randomizedArray = [];
+  // perform shuffle
+  while (cloned.length !== 0) {
+    let rIndex = Math.floor(cloned.length * Math.random());
+    randomizedArray.push(cloned[rIndex]);
+    cloned.splice(rIndex, 1)
+  }
+  return randomizedArray;
+};
+
+export { shuffle };
