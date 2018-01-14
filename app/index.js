18d3544ff5fb842527d0501d752b32f0ab1dcde4
Yiotis Kaltsikis <giotisgr@gmail.com>
2018-01-14 18:24:05 +0100

    Add author related comments

--- app/index.js
+++ app/index.js
@@ -1,3 +1,13 @@
+/**
+* Filterizr is a jQuery plugin that sorts, shuffles and applies stunning filters over
+* responsive galleries using CSS3 transitions and custom CSS effects.
+*
+* @author Yiotis Kaltsikis
+* @see {@link http://yiotis.net/filterizr}
+* @version 1.3.0
+* @license MIT License
+*/
+
 import Filterizr from './Filterizr';
 import DefaultOptions from './options';
 
