d3bbebe7b97433511f26e586ad2785390dccbb85
Yiotis Kaltsikis <giotisgr@gmail.com>
2019-06-21 20:42:51 +0200

    Set up react router

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,14 +1,17 @@
 import React from 'react';
+import { HashRouter as Router, Route } from 'react-router-dom';
 import Navigation from '../Navigation';
 import ShowcasePage from '../ShowcasePage';
 
 function App() {
   return (
     <div className="App">
-      <Navigation />
-      <main>
-        <ShowcasePage />
-      </main>
+      <Router>
+        <Navigation />
+        <main>
+          <Route path="/" exact component={ShowcasePage} />
+        </main>
+      </Router>
     </div>
   );
 }
--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Dropdown, Image, Menu, Segment } from 'semantic-ui-react';
+import { Link } from 'react-router-dom';
 
 import './Navigation.scss';
 
@@ -14,37 +15,98 @@ export default class Navigation extends Component {
     return (
       <Segment className="Navigation" inverted>
         <Menu inverted className="Navigation__navbar">
-          <Menu.Menu className="Navigation__logo" position="left">
+          <Menu.Menu
+            as={Link}
+            to="/"
+            onClick={(event) =>
+              this.handleItemClick(event, { name: 'showcase' })
+            }
+            className="Navigation__logo"
+            position="left"
+          >
             <Image src="/img/filterizr-logo.png" fluid />
           </Menu.Menu>
           <Menu.Menu position="right">
             <Menu.Item
+              as={Link}
+              to="/"
               name="showcase"
               active={activeItem === 'showcase'}
               onClick={this.handleItemClick}
             />
             <Dropdown
               active={activeItem === 'tutorials'}
-              onClick={this.handleItemClick}
               as={Menu.Item}
               item
               text="Tutorials"
             >
               <Dropdown.Menu>
-                <Dropdown.Item>Installing</Dropdown.Item>
-                <Dropdown.Item>Filtering</Dropdown.Item>
-                <Dropdown.Item>Sorting</Dropdown.Item>
-                <Dropdown.Item>Searching</Dropdown.Item>
-                <Dropdown.Item>Delay modes</Dropdown.Item>
-                <Dropdown.Item>Layouts</Dropdown.Item>
+                <Dropdown.Item
+                  as={Link}
+                  to="/tutorials/installing"
+                  onClick={(event) =>
+                    this.handleItemClick(event, { name: 'tutorials' })
+                  }
+                >
+                  Installing
+                </Dropdown.Item>
+                <Dropdown.Item
+                  as={Link}
+                  to="/tutorials/filtering"
+                  onClick={(event) =>
+                    this.handleItemClick(event, { name: 'tutorials' })
+                  }
+                >
+                  Filtering
+                </Dropdown.Item>
+                <Dropdown.Item
+                  as={Link}
+                  to="/tutorials/sorting"
+                  onClick={(event) =>
+                    this.handleItemClick(event, { name: 'tutorials' })
+                  }
+                >
+                  Sorting
+                </Dropdown.Item>
+                <Dropdown.Item
+                  as={Link}
+                  to="/tutorials/searching"
+                  onClick={(event) =>
+                    this.handleItemClick(event, { name: 'tutorials' })
+                  }
+                >
+                  Searching
+                </Dropdown.Item>
+                <Dropdown.Item
+                  as={Link}
+                  to="/tutorials/delay-modes"
+                  onClick={(event) =>
+                    this.handleItemClick(event, { name: 'tutorials' })
+                  }
+                >
+                  Delay modes
+                </Dropdown.Item>
+                <Dropdown.Item
+                  as={Link}
+                  to="/tutorials/layouts"
+                  onClick={(event) =>
+                    this.handleItemClick(event, { name: 'tutorials' })
+                  }
+                >
+                  Layouts
+                </Dropdown.Item>
               </Dropdown.Menu>
             </Dropdown>
             <Menu.Item
+              as={Link}
+              to="/documentation"
               name="docs"
               active={activeItem === 'docs'}
               onClick={this.handleItemClick}
             />
             <Menu.Item
+              as={Link}
+              to="/faq"
               name="FAQ"
               active={activeItem === 'FAQ'}
               onClick={this.handleItemClick}
