95a28d04db2ac6c59858feffe87494cea20631d4
Yiotis Kaltsikis <giotisgr@gmail.com>
2019-06-22 18:49:44 +0200

    Implement sorting tutorial

--- src/components/SortingTutorialPage/index.js
+++ src/components/SortingTutorialPage/index.js
@@ -1,26 +1,51 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { Grid, Header } from 'semantic-ui-react';
 import Filterizr from '../Filterizr';
 import Highlight from 'react-highlight';
-import ColoredFilteritems from '../ColoredFilterItems';
 
 import './SortingTutorialPage.scss';
 
-const snippetStep1 = `<ul>
-  <li data-filter="all"> All </li>
-  <li data-filter="green"> Green </li>
-  <li data-filter="orange"> Orange </li>
-  <li data-filter="purple"> Purple </li>
-  <li data-filter="orange, purple"> Mix </li>
-</ul>
+const snippet1 = `<select data-sortOrder>
+  <option value="domIndex">Order it appears in DOM</option>
+  <option value="sortData">Value of data-sort attribute</option>
+  <!-- Next two can be used only for layouts of varying widths/heights -->
+  <option value="w">Item Width</option>
+  <option value="h">Item Height</option>
+</select>
 `;
 
-const snippetStep2 = `<ul>
-  <li data-multifilter="all"> All </li>
-  <li data-multifilter="1"> Green </li>
-  <li data-multifilter="2"> Orange </li>
-  <li data-multifilter="3"> Purple </li>
-</ul>`;
+const snippet2 = `//Example 1: sort by item width, descending.
+filterizr.sort('w', 'desc');
+//Example 2: sort by the value of data-sort attribute, ascending.
+filterizr.sort('sortData', 'asc');
+`;
+
+const snippet3 = `<div class="filtr-container">
+  <div class="filtr-item" data-category="1" data-author="John Doe" data-year="1998" data-novel="Cool book 1">
+    <img src="img/sample.jpg" alt="sample">
+  </div>
+  <div class="filtr-item" data-category="1" data-author="Jane Doe" data-year="2003" data-novel="Cooler book">
+    <img src="img/sample.jpg" alt="sample">
+  </div>
+  <div class="filtr-item" data-category="1" data-author="Jake Doe" data-year="2008" data-novel="Coolest book">
+    <img src="img/sample.jpg" alt="sample">
+  </div>
+</div>`;
+
+const snippet4 = `<select data-sortOrder>
+  <option value="domIndex"> Position in DOM </option>
+  <!-- Other options and then... -->
+  <option value="author">Author Name</option>
+  <option value="year">Year Published</option>
+  <option value="novel">Book Title</option>
+</select>`;
+
+const snippet5 = `//Example 1: sort by author name, descending.
+filterizr.sort('author', 'desc');
+//Example 2: sort by book title, ascending.
+filterizr.sort('novel', 'asc');
+`;
 
 class SortingTutorialPage extends React.Component {
   render() {
@@ -28,82 +53,213 @@ class SortingTutorialPage extends React.Component {
       <Grid className="SortingTutorialPage" divided="vertically">
         <Grid.Row columns={1}>
           <Grid.Column>
-            <Header as="h1">Filtering</Header>
+            <Header as="h1">Sorting</Header>
             <p>
-              Filterizr offers two filtering modes, the active filter mode and
-              the toggle filter mode. All you have to do is use the appropriate
-              preset Filterizr control for each one of the options and Filterizr
-              will handle the rest for you. In the section below you can find
-              examples of both modes, what they look like as well as how to set
-              them up.
+              Filterizr allows you to sort your grid in two ways. You can either
+              use the default preset options that work out of the box, or define
+              your own custom data-attribute and use them to sort.
             </p>
           </Grid.Column>
         </Grid.Row>
         <Grid.Row columns={1}>
-          <Grid.Column mobile={16} computer={8}>
-            <Header as="h2">Active filter mode</Header>
-            <Header as="h3">Description</Header>
-            <p className="hljs-inline">
-              For this filtering mode all you need to do is to include the
-              <Highlight className="html">data-filter</Highlight> attribute in
-              your controls as illustrated below. Each time you switch between
-              filters by clicking a button, items of the corresponding{' '}
-              <Highlight className="html">data-category</Highlight> will be
-              shown. This mode uses the{' '}
-              <Highlight className="javascript">
-                Filterizr.prototype.filter(targetFilter)
-              </Highlight>{' '}
-              method in the background to switch between categories.
-            </p>
-            <Header as="h3">Control setup</Header>
-            <Highlight className="html">{snippetStep1}</Highlight>
-          </Grid.Column>
-          <Grid.Column mobile={16} computer={8}>
+          <Grid.Column>
+            <Header as="h2">Example</Header>
             <Filterizr
-              filterControls={[
-                <Filterizr.FilterButton text="All" targetFilter="all" />,
-                <Filterizr.FilterButton text="Orange" targetFilter="orange" />,
-                <Filterizr.FilterButton text="Green" targetFilter="green" />,
-                <Filterizr.FilterButton text="Purple" targetFilter="purple" />,
-                <Filterizr.FilterButton
-                  text="Mix"
-                  targetFilter={['purple', 'orange']}
-                />,
+              sortAttributes={[
+                { key: 'domIndex', value: 'domIndex', text: 'Index' },
+                { key: 'black', value: 'black', text: 'Black letter' },
+                { key: 'blue', value: 'blue', text: 'Blue letter' },
               ]}
-              selector=".active-filter"
+              sortable
+              selector=".sortable-grid"
             >
-              <ColoredFilteritems />
+              <div
+                className="filtr-item"
+                data-black="A"
+                data-blue="X"
+                data-category="orange"
+              >
+                <span className="colored orange">
+                  1-
+                  <span className="black-letter">A</span>-
+                  <span className="blue-letter">X</span>
+                </span>
+              </div>
+              <div
+                className="filtr-item"
+                data-black="D"
+                data-blue="F"
+                data-category="green"
+              >
+                <span className="colored green">
+                  2-
+                  <span className="black-letter">D</span>-
+                  <span className="blue-letter">F</span>
+                </span>
+              </div>
+              <div
+                className="filtr-item"
+                data-black="E"
+                data-blue="Z"
+                data-category="purple"
+              >
+                <span className="colored purple">
+                  3-
+                  <span className="black-letter">E</span>-
+                  <span className="blue-letter">Z</span>
+                </span>
+              </div>
+              <div
+                className="filtr-item"
+                data-black="C"
+                data-blue="H"
+                data-category="green"
+              >
+                <span className="colored green">
+                  4-
+                  <span className="black-letter">C</span>-
+                  <span className="blue-letter">H</span>
+                </span>
+              </div>
+              <div
+                className="filtr-item"
+                data-black="G"
+                data-blue="J"
+                data-category="orange"
+              >
+                <span className="colored orange">
+                  5-
+                  <span className="black-letter">G</span>-
+                  <span className="blue-letter">J</span>
+                </span>
+              </div>
+              <div
+                className="filtr-item"
+                data-black="R"
+                data-blue="Y"
+                data-category="purple"
+              >
+                <span className="colored purple">
+                  6-
+                  <span className="black-letter">R</span>-
+                  <span className="blue-letter">Y</span>
+                </span>
+              </div>
+              <div
+                className="filtr-item"
+                data-black="M"
+                data-blue="L"
+                data-category="orange"
+              >
+                <span className="colored orange">
+                  7-
+                  <span className="black-letter">M</span>-
+                  <span className="blue-letter">L</span>
+                </span>
+              </div>
+              <div
+                className="filtr-item"
+                data-black="M"
+                data-blue="Q"
+                data-category="purple"
+              >
+                <span className="colored purple">
+                  8-
+                  <span className="black-letter">K</span>-
+                  <span className="blue-letter">Q</span>
+                </span>
+              </div>
+              <div
+                className="filtr-item"
+                data-black="W"
+                data-blue="V"
+                data-category="green"
+              >
+                <span className="colored green">
+                  9-
+                  <span className="black-letter">W</span>-
+                  <span className="blue-letter">V</span>
+                </span>
+              </div>
+              <div
+                className="filtr-item"
+                data-black="P"
+                data-blue="U"
+                data-category="purple"
+              >
+                <span className="colored purple">
+                  10-
+                  <span className="black-letter">P</span>-
+                  <span className="blue-letter">U</span>
+                </span>
+              </div>
+              <div
+                className="filtr-item"
+                data-black="O"
+                data-blue="S"
+                data-category="orange"
+              >
+                <span className="colored orange">
+                  11-
+                  <span className="black-letter">O</span>-
+                  <span className="blue-letter">S</span>
+                </span>
+              </div>
+              <div
+                className="filtr-item"
+                data-black="I"
+                data-blue="T"
+                data-category="green"
+              >
+                <span className="colored green">
+                  12-
+                  <span className="black-letter">I</span>-
+                  <span className="blue-letter">T</span>
+                </span>
+              </div>
             </Filterizr>
           </Grid.Column>
         </Grid.Row>
+
         <Grid.Row columns={1}>
           <Grid.Column mobile={16} computer={8}>
-            <Header as="h2">Toggle filter mode</Header>
-            <Header as="h3">Description</Header>
-            <p className="hljs-inline">
-              For this filtering mode you need to include the{' '}
-              <Highlight className="html">data-multifilter</Highlight> attribute
-              in your controls as illustrated below. When all buttons are
-              switched off an unfiltered gallery is shown. This mode uses the{' '}
-              <Highlight className="javascript">
-                Filterizr.prototype.toggleFilter(toggledFilter)
-              </Highlight>{' '}
-              method in the background to hide or display categories.
+            <Header as="h2">Basic sorting</Header>
+            <p>
+              Filterizr has some preset options for sorting. In the{' '}
+              <Link to="/tutorials/quickstart">Get started</Link> tutorial, in
+              the default sorting controls a select input element is used to
+              determine the value by which Filterizr's elements are ordered:
             </p>
-            <Header as="h3">Control setup</Header>
-            <Highlight className="html">{snippetStep2}</Highlight>
+            <Highlight className="html">{snippet1}</Highlight>
+            <p>
+              Then you can either use Filterizr's preset controls to sort your
+              elements by that value, or call the corresponding public method
+              like this:
+            </p>
+            <Highlight className="javascript">{snippet2}</Highlight>
           </Grid.Column>
           <Grid.Column mobile={16} computer={8}>
-            <Filterizr
-              multiFilterControls={[
-                <Filterizr.FilterButton text="Orange" targetFilter="orange" />,
-                <Filterizr.FilterButton text="Green" targetFilter="green" />,
-                <Filterizr.FilterButton text="Purple" targetFilter="purple" />,
-              ]}
-              selector=".toggle-filter"
-            >
-              <ColoredFilteritems />
-            </Filterizr>
+            <Header as="h2">Advanced sorting</Header>
+            <p>
+              If you want to use custom values by which to sort your elements,
+              Filterizr allows you to add arbitrary data-attributes with custom
+              names and values to your items and use them for sorting. For
+              example you could have a grid looking like this:
+            </p>
+            <Highlight className="html">{snippet3}</Highlight>
+            <p>
+              Then all you have to do is add those private data-attribute names
+              to your select input element:
+            </p>
+            <Highlight className="html">{snippet4}</Highlight>
+            <p className="hljs-inline">
+              If you are using the default sorting controls that's it! If you
+              wish to use the public API once again you simply pass the value of
+              the option elements as the sort attribute parameter. Just remember
+              to omit the <Highlight className="html">data-</Highlight> part.
+              For example:
+            </p>
+            <Highlight className="javascript">{snippet5}</Highlight>
           </Grid.Column>
         </Grid.Row>
       </Grid>
