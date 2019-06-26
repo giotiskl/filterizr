d758f9184fa6f38f0decb3f116bbc34a2a3972b2
Yiotis Kaltsikis <giotisgr@gmail.com>
2019-06-26 11:27:08 +0200

    Updates

---
+++ precache-manifest.0eb50e5c35daa13afa76e4de417dac37.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4224a5c63ea25de6bf52d7a5f6f11728",
+    "url": "/filterizr/index.html"
+  },
+  {
+    "revision": "80793570edef6fdf9482",
+    "url": "/filterizr/static/css/2.06496d04.chunk.css"
+  },
+  {
+    "revision": "3327ae25ba21cb5d50d6",
+    "url": "/filterizr/static/css/main.c6e551fe.chunk.css"
+  },
+  {
+    "revision": "80793570edef6fdf9482",
+    "url": "/filterizr/static/js/2.d0592b4d.chunk.js"
+  },
+  {
+    "revision": "3327ae25ba21cb5d50d6",
+    "url": "/filterizr/static/js/main.0ece2c6a.chunk.js"
+  },
+  {
+    "revision": "00425ff2de68cb267ebc",
+    "url": "/filterizr/static/js/runtime~main.0e01647f.js"
+  },
+  {
+    "revision": "d961fdfabbe512d2675de09af09f598b",
+    "url": "/filterizr/static/media/jsoneditor-icons.d961fdfa.svg"
+  }
+]);
