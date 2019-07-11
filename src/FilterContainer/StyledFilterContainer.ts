9297726f7db0b8ec01d191c388ed52da8b98cc3a
Yiotis Kaltsikis <giotisgr@gmail.com>
2019-07-11 15:56:48 +0200

    Remove redundant constructor

--- src/FilterContainer/StyledFilterContainer.ts
+++ src/FilterContainer/StyledFilterContainer.ts
@@ -4,13 +4,8 @@ import {
   makeHeightStyles,
 } from './styles';
 import StyledFilterizrElement from '../StyledFilterizrElement';
-import FilterizrOptions from '../FilterizrOptions';
 
 export default class StyledFilterContainer extends StyledFilterizrElement {
-  public constructor(node: HTMLElement, options: FilterizrOptions) {
-    super(node, options);
-  }
-
   public initialize(): void {
     this.set(makeInitialStyles(this.options));
   }
--- src/Spinner/StyledSpinner.ts
+++ src/Spinner/StyledSpinner.ts
@@ -1,11 +1,6 @@
 import StyledFilterizrElement from '../StyledFilterizrElement';
-import FilterizrOptions from '../FilterizrOptions';
 
 export default class StyledSpinner extends StyledFilterizrElement {
-  public constructor(node: HTMLElement, options: FilterizrOptions) {
-    super(node, options);
-  }
-
   public initialize(): void {
     const { styles } = this.options.get().spinner;
     this.set({
