c6843ce9c789241052114b6280732836ccf6f214
Yiotis Kaltsikis <giotisgr@gmail.com>
2019-07-14 19:41:01 +0200

    Memoize the factory of layout positions

--- src/makeLayoutPositions/makeLayoutPositions.ts
+++ src/makeLayoutPositions/makeLayoutPositions.ts
@@ -1,4 +1,5 @@
 import { ContainerLayout, Dimensions, Options } from '../types/interfaces';
+import memoize from 'fast-memoize';
 import makeHorizontalLayoutPositions from './makeHorizontalLayoutPositions';
 import makeVerticalLayoutPositions from './makeVerticalLayoutPositions';
 import makeSameHeightLayoutPositions from './makeSameHeightLayoutPositions';
@@ -7,60 +8,60 @@ import makeSameSizeLayoutPosition from './makeSameSizeLayoutPosition';
 import makePackedLayoutPositions from './makePackedLayoutPositions';
 
 /**
- * Calculates and returns an array of objects representing
- * the next positions the FilterItems are supposed to assume.
- * @param layout name of helper method to be used
- * @param filterizr instance
+ * Creates the specifications of the dimensions of the
+ * container and items for the next render of Filterizr.
  */
-export default (
-  containerWidth: number,
-  itemsDimensions: Dimensions[],
-  { gutterPixels, layout }: Options
-): ContainerLayout => {
-  if (!itemsDimensions.length) {
-    return {
-      containerHeight: 0,
-      itemsPositions: [],
-    };
-  }
+export default memoize(
+  (
+    containerWidth: number,
+    itemsDimensions: Dimensions[],
+    { gutterPixels, layout }: Options
+  ): ContainerLayout => {
+    if (!itemsDimensions.length) {
+      return {
+        containerHeight: 0,
+        itemsPositions: [],
+      };
+    }
 
-  switch (layout) {
-    case 'horizontal':
-      return makeHorizontalLayoutPositions(
-        containerWidth,
-        itemsDimensions,
-        gutterPixels
-      );
-    case 'vertical':
-      return makeVerticalLayoutPositions(
-        containerWidth,
-        itemsDimensions,
-        gutterPixels
-      );
-    case 'sameHeight':
-      return makeSameHeightLayoutPositions(
-        containerWidth,
-        itemsDimensions,
-        gutterPixels
-      );
-    case 'sameWidth':
-      return makeSameWidthLayoutPositions(
-        containerWidth,
-        itemsDimensions,
-        gutterPixels
-      );
-    case 'packed':
-      return makePackedLayoutPositions(
-        containerWidth,
-        itemsDimensions,
-        gutterPixels
-      );
-    case 'sameSize':
-    default:
-      return makeSameSizeLayoutPosition(
-        containerWidth,
-        itemsDimensions,
-        gutterPixels
-      );
+    switch (layout) {
+      case 'horizontal':
+        return makeHorizontalLayoutPositions(
+          containerWidth,
+          itemsDimensions,
+          gutterPixels
+        );
+      case 'vertical':
+        return makeVerticalLayoutPositions(
+          containerWidth,
+          itemsDimensions,
+          gutterPixels
+        );
+      case 'sameHeight':
+        return makeSameHeightLayoutPositions(
+          containerWidth,
+          itemsDimensions,
+          gutterPixels
+        );
+      case 'sameWidth':
+        return makeSameWidthLayoutPositions(
+          containerWidth,
+          itemsDimensions,
+          gutterPixels
+        );
+      case 'packed':
+        return makePackedLayoutPositions(
+          containerWidth,
+          itemsDimensions,
+          gutterPixels
+        );
+      case 'sameSize':
+      default:
+        return makeSameSizeLayoutPosition(
+          containerWidth,
+          itemsDimensions,
+          gutterPixels
+        );
+    }
   }
-};
+);
